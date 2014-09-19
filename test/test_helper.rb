ded23f59e5637f88136a212ad1c762a09412dfac
pseudomuto <david.muto@gmail.com>
2014-09-19 19:10:16 +0000

    rescuing LoadError when requiring pry

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,10 @@ ENV["RAILS_ENV"] = "test"
 
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
-require "pry"
+
+begin
+  require "pry"
+rescue LoadError
+end
 
 Rails.backtrace_cleaner.remove_silencers!
