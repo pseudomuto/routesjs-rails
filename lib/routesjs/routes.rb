252613ff491105f84d401d4460fc5ab9bd8a7fee
pseudomuto <david.muto@gmail.com>
2014-09-19 15:12:58 +0000

    using Rails.application from the global namespace

--- lib/routesjs/routes.rb
+++ lib/routesjs/routes.rb
@@ -1,7 +1,7 @@
 module RoutesJS
   class Routes
     def self.as_json(routes = nil)
-      routes ||= Rails.application.routes.routes
+      routes ||= ::Rails.application.routes.routes
       inspector = ActionDispatch::Routing::RoutesInspector.new(routes)
       inspector.format(Formatter.new)
     end
