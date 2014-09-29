6a7c5acb6c21e43f1dcf05fecb13ad78e087433f
pseudomuto <david.muto@gmail.com>
2014-09-29 23:21:39 +0000

    dropping dat prototype extension mess

--- test/javascripts/routesjs-rails_spec.js
+++ test/javascripts/routesjs-rails_spec.js
@@ -15,18 +15,18 @@ describe("Routes", function() {
     });
 
     it("makes a path function for each route", function() {
-      expect(Routes.oauthTokenPath()).toBe("/oauth/token");
-      expect(Routes.adminRootPath()).toBe("/admin");
+      expect(Routes.oauthTokenPath()).toEqual("/oauth/token");
+      expect(Routes.adminRootPath()).toEqual("/admin");
     });
 
     it("makes a url function for each route", function() {
-      expect(Routes.oauthTokenUrl()).toBe("//example.com/oauth/token");
-      expect(Routes.adminRootUrl()).toBe("//example.com/admin");
+      expect(Routes.oauthTokenUrl()).toEqual("//example.com/oauth/token");
+      expect(Routes.adminRootUrl()).toEqual("//example.com/admin");
     });
 
     it("returns the raw route for both path and url when absolute", function() {
-      expect(Routes.googlePath()).toBe("https://www.google.com/");
-      expect(Routes.googleUrl()).toBe("https://www.google.com/");
+      expect(Routes.googlePath()).toEqual("https://www.google.com/");
+      expect(Routes.googleUrl()).toEqual("https://www.google.com/");
     });
   });
 
@@ -47,9 +47,9 @@ describe("Routes", function() {
         role_id: 2
       };
 
-      expect(Routes.userPath(user)).toBe("/users/1");
-      expect(Routes.userRolesPath(user)).toBe("/users/1/roles");
-      expect(Routes.userRoleUrl(user)).toBe("//example.com/users/1/roles/2");
+      expect(Routes.userPath(user)).toEqual("/users/1");
+      expect(Routes.userRolesPath(user)).toEqual("/users/1/roles");
+      expect(Routes.userRoleUrl(user)).toEqual("//example.com/users/1/roles/2");
     });
   });
 
@@ -65,16 +65,16 @@ describe("Routes", function() {
     });
 
     it("replaces tokens with ordered arguments", function() {
-      expect(Routes.userPath(1)).toBe("/users/1");
-      expect(Routes.userRoleUrl(1, "admin")).toBe("//example.com/users/1/roles/admin");
+      expect(Routes.userPath(1)).toEqual("/users/1");
+      expect(Routes.userRoleUrl(1, "admin")).toEqual("//example.com/users/1/roles/admin");
     });
 
     it("ignores extra arguments", function() {
-      expect(Routes.userPath(1, "this", 2, "be", "ignored")).toBe("/users/1");
+      expect(Routes.userPath(1, "this", 2, "be", "ignored")).toEqual("/users/1");
     });
 
     it("returns the unmodified route when no tokens are found", function() {
-      expect(Routes.rootPath(1, 2)).toBe("/");
+      expect(Routes.rootPath(1, 2)).toEqual("/");
     });
   });
 
@@ -90,15 +90,15 @@ describe("Routes", function() {
     });
 
     it("uses the format property when specified", function() {
-      expect(Routes.loginPath({ format: "html" })).toBe("/login.html");
-      expect(Routes.userPath({ id: 1, format: "json" })).toBe("/users/1.json");
-      expect(Routes.userRolePath({ id: 1, role_id: 2, format: "json" })).toBe("/users/1/roles/2.json");
+      expect(Routes.loginPath({ format: "html" })).toEqual("/login.html");
+      expect(Routes.userPath({ id: 1, format: "json" })).toEqual("/users/1.json");
+      expect(Routes.userRolePath({ id: 1, role_id: 2, format: "json" })).toEqual("/users/1/roles/2.json");
     });
 
     it("overrides the format when modifier method is called", function() {
-      expect(Routes.loginPath({ format: "html" }).json()).toBe("/login.json");
-      expect(Routes.userPath({ id: 1, format: "json" }).html()).toBe("/users/1.html");
-      expect(Routes.userRolePath({ id: 1, role_id: 2, format: "json" }).html()).toBe("/users/1/roles/2.html");
+      expect(Routes.loginPath({ format: "html" }).json()).toEqual("/login.json");
+      expect(Routes.userPath({ id: 1, format: "json" }).html()).toEqual("/users/1.html");
+      expect(Routes.userRolePath({ id: 1, role_id: 2, format: "json" }).html()).toEqual("/users/1/roles/2.html");
     });
   });
 
@@ -115,20 +115,20 @@ describe("Routes", function() {
     });
 
     it("applies the default format to routes", function() {
-      expect(Routes.loginPath()).toBe("/login.json");
-      expect(Routes.userPath(1)).toBe("/users/1.json");
-      expect(Routes.userRolePath(1, 2)).toBe("/users/1/roles/2.json");
+      expect(Routes.loginPath()).toEqual("/login.json");
+      expect(Routes.userPath(1)).toEqual("/users/1.json");
+      expect(Routes.userRolePath(1, 2)).toEqual("/users/1/roles/2.json");
     });
 
     it("allows overriding the default format", function() {
-      expect(Routes.loginPath({ format: "html" })).toBe("/login.html");
-      expect(Routes.userPath({ format: "html", id: 1 })).toBe("/users/1.html");
-      expect(Routes.userRolePath({ format: "html", id: 1, role_id: 2 })).toBe("/users/1/roles/2.html");
+      expect(Routes.loginPath({ format: "html" })).toEqual("/login.html");
+      expect(Routes.userPath({ format: "html", id: 1 })).toEqual("/users/1.html");
+      expect(Routes.userRolePath({ format: "html", id: 1, role_id: 2 })).toEqual("/users/1/roles/2.html");
     });
 
     it("respects format modifiers", function() {
-      expect(Routes.userPath({ id: 1, format: "html" }).xml()).toBe("/users/1.xml");
-      expect(Routes.userPath(1).none()).toBe("/users/1");
+      expect(Routes.userPath({ id: 1, format: "html" }).xml()).toEqual("/users/1.xml");
+      expect(Routes.userPath(1).none()).toEqual("/users/1");
     });
   });
 });
