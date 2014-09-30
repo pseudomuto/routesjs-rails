cbc694f7b120d44ec1474138dccfb32886787882
pseudomuto <david.muto@gmail.com>
2014-09-30 11:45:50 +0000

    updates for rails 4.2.0 beta 2

--- test/generators/tmp/config/initializers/routesjs_rails.rb
+++
@@ -1,7 +0,0 @@
-# The default format to use for routes. The default value is nil (no extension added)
-
-# Will add .html to generated routes
-#RoutesJS::Routes.default_format = "html"
-
-# Will add .json to generated routes
-#RoutesJS::Routes.default_format = "json"
