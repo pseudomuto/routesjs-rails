8992bee390401f1c97462d39e3d161e65e2701f4
pseudomuto <david.muto@gmail.com>
2014-10-01 02:38:46 +0000

    adding toRouteTo matcher to make it all pretty-like

--- test/javascripts/routesjs-rails_spec.js
+++ test/javascripts/routesjs-rails_spec.js
@@ -1,5 +1,9 @@
 describe("Routes", function() {
   beforeEach(function() {
+    jasmine.Matchers.prototype.toRouteTo = function(expected) {
+      return this.actual === expected;
+    };
+
     Routes.setBaseUrl("//example.com/");
   });
 
@@ -15,18 +19,18 @@ describe("Routes", function() {
     });
 
     it("makes a path function for each route", function() {
-      expect(Routes.oauthTokenPath().toString()).toEqual("/oauth/token");
-      expect(Routes.adminRootPath().toString()).toEqual("/admin");
+      expect(Routes.oauthTokenPath()).toRouteTo("/oauth/token");
+      expect(Routes.adminRootPath()).toRouteTo("/admin");
     });
 
     it("makes a url function for each route", function() {
-      expect(Routes.oauthTokenUrl().toString()).toEqual("//example.com/oauth/token");
-      expect(Routes.adminRootUrl().toString()).toEqual("//example.com/admin");
+      expect(Routes.oauthTokenUrl()).toRouteTo("//example.com/oauth/token");
+      expect(Routes.adminRootUrl()).toRouteTo("//example.com/admin");
     });
 
     it("returns the raw route for both path and url when absolute", function() {
-      expect(Routes.googlePath().toString()).toEqual("https://www.google.com/");
-      expect(Routes.googleUrl().toString()).toEqual("https://www.google.com/");
+      expect(Routes.googlePath()).toRouteTo("https://www.google.com/");
+      expect(Routes.googleUrl()).toRouteTo("https://www.google.com/");
     });
   });
 
@@ -47,9 +51,9 @@ describe("Routes", function() {
         role_id: 2
       };
 
-      expect(Routes.userPath(user).toString()).toEqual("/users/1");
-      expect(Routes.userRolesPath(user).toString()).toEqual("/users/1/roles");
-      expect(Routes.userRoleUrl(user).toString()).toEqual("//example.com/users/1/roles/2");
+      expect(Routes.userPath(user)).toRouteTo("/users/1");
+      expect(Routes.userRolesPath(user)).toRouteTo("/users/1/roles");
+      expect(Routes.userRoleUrl(user)).toRouteTo("//example.com/users/1/roles/2");
     });
   });
 
@@ -65,16 +69,16 @@ describe("Routes", function() {
     });
 
     it("replaces tokens with ordered arguments", function() {
-      expect(Routes.userPath(1).toString()).toEqual("/users/1");
-      expect(Routes.userRoleUrl(1, "admin").toString()).toEqual("//example.com/users/1/roles/admin");
+      expect(Routes.userPath(1)).toRouteTo("/users/1");
+      expect(Routes.userRoleUrl(1, "admin")).toRouteTo("//example.com/users/1/roles/admin");
     });
 
     it("ignores extra arguments", function() {
-      expect(Routes.userPath(1, "this", 2, "be", "ignored").toString()).toEqual("/users/1");
+      expect(Routes.userPath(1, "this", 2, "be", "ignored")).toRouteTo("/users/1");
     });
 
     it("returns the unmodified route when no tokens are found", function() {
-      expect(Routes.rootPath(1, 2).toString()).toEqual("/");
+      expect(Routes.rootPath(1, 2)).toRouteTo("/");
     });
   });
 
@@ -90,15 +94,15 @@ describe("Routes", function() {
     });
 
     it("uses the format property when specified", function() {
-      expect(Routes.loginPath({ format: "html" }).toString()).toEqual("/login.html");
-      expect(Routes.userPath({ id: 1, format: "json" }).toString()).toEqual("/users/1.json");
-      expect(Routes.userRolePath({ id: 1, role_id: 2, format: "json" }).toString()).toEqual("/users/1/roles/2.json");
+      expect(Routes.loginPath({ format: "html" })).toRouteTo("/login.html");
+      expect(Routes.userPath({ id: 1, format: "json" })).toRouteTo("/users/1.json");
+      expect(Routes.userRolePath({ id: 1, role_id: 2, format: "json" })).toRouteTo("/users/1/roles/2.json");
     });
 
     it("overrides the format when modifier method is called", function() {
-      expect(Routes.loginPath({ format: "html" }).json().toString()).toEqual("/login.json");
-      expect(Routes.userPath({ id: 1, format: "json" }).html().toString()).toEqual("/users/1.html");
-      expect(Routes.userRolePath({ id: 1, role_id: 2, format: "json" }).html().toString()).toEqual("/users/1/roles/2.html");
+      expect(Routes.loginPath({ format: "html" }).json()).toRouteTo("/login.json");
+      expect(Routes.userPath({ id: 1, format: "json" }).html()).toRouteTo("/users/1.html");
+      expect(Routes.userRolePath({ id: 1, role_id: 2, format: "json" }).html()).toRouteTo("/users/1/roles/2.html");
     });
   });
 
@@ -115,20 +119,20 @@ describe("Routes", function() {
     });
 
     it("applies the default format to routes", function() {
-      expect(Routes.loginPath().toString()).toEqual("/login.json");
-      expect(Routes.userPath(1).toString()).toEqual("/users/1.json");
-      expect(Routes.userRolePath(1, 2).toString()).toEqual("/users/1/roles/2.json");
+      expect(Routes.loginPath()).toRouteTo("/login.json");
+      expect(Routes.userPath(1)).toRouteTo("/users/1.json");
+      expect(Routes.userRolePath(1, 2)).toRouteTo("/users/1/roles/2.json");
     });
 
     it("allows overriding the default format", function() {
-      expect(Routes.loginPath({ format: "html" }).toString()).toEqual("/login.html");
-      expect(Routes.userPath({ format: "html", id: 1 }).toString()).toEqual("/users/1.html");
-      expect(Routes.userRolePath({ format: "html", id: 1, role_id: 2 }).toString()).toEqual("/users/1/roles/2.html");
+      expect(Routes.loginPath({ format: "html" })).toRouteTo("/login.html");
+      expect(Routes.userPath({ format: "html", id: 1 })).toRouteTo("/users/1.html");
+      expect(Routes.userRolePath({ format: "html", id: 1, role_id: 2 })).toRouteTo("/users/1/roles/2.html");
     });
 
     it("respects format modifiers", function() {
-      expect(Routes.userPath({ id: 1, format: "html" }).xml().toString()).toEqual("/users/1.xml");
-      expect(Routes.userPath(1).none().toString()).toEqual("/users/1");
+      expect(Routes.userPath({ id: 1, format: "html" }).xml()).toRouteTo("/users/1.xml");
+      expect(Routes.userPath(1).none()).toRouteTo("/users/1");
     });
   });
 });
